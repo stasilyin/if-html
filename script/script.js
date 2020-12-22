ffb454137c7390182b545d3192553f01caecfec6
stasilyin <stasilyin.by@gmail.com>
2020-12-23 01:19:58 +0300

    add animation

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-$('').on('click', function(e){
+$('header-img-btn__mobile_btn').on('click', function(e){
     e.preventDefault;
     $this.toggleClass('mobile-btn-active');
     $('.header-menu__link').toggleClass('header-menu__link-activ');
