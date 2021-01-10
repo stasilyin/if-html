d5fd4aa1ab6046886788ee54887b5eacdd326f20
stasilyin <stasilyin.by@gmail.com>
2021-01-11 01:36:43 +0300

    add homework 10

--- script/script.js
+++ script/script.js
@@ -1,5 +1,25 @@
+$(document).ready(function() {
 $('.header-img-btn__mobile_btn').on('click', function(e){
-    e.preventDefault;
+    e.preventDefault();
     $(this).toggleClass('mobile-btn-active');
     $('.header-menu__link').toggleClass('header-menu__link-activ');
+});
+$('.btn-slider').on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('btn-slider-active');
+    $('.distenation-image').toggleClass('distenation-image-active');
+});
+$('.distenation-tab__item').on('click', function(e){
+    e.preventDefault();
+    $('.distenation-tab__item').removeClass('distenation-tab__item--active');
+    $('.distenation-tab__content').removeClass('distenation-tab__content--active');
+    $(this).addClass('distenation-tab__item--active');
+    $($(this).attr('href')).addClass('distenation-tab__content--active');
+});
+$('.distenation-banner__close').on('click', function(e){
+    e.preventDefault();
+    $('.distenation-banner').toggleClass('distenation-banner--hiden');
+    setTimeout(function () {$('.distenation-banner').toggleClass('distenation-banner--disable');}, 500);
+});
+$('.distenation-tab__item:first').click();
 });
